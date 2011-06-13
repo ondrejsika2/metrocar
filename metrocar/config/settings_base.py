53a9d38738cb60cb0fc02fa6459cef86d1f51a22
Jan Wagner <wagnejan@fel.cvut.cz>
2011-06-13 03:42:47 +0000

    Zakaz vypisu debugovacich hlasek pri chybe Zmena kontaktnich udaju administratora

--- metrocar/config/settings_base.py
+++ metrocar/config/settings_base.py
@@ -9,17 +9,10 @@ TEMPLATE_DEBUG = DEBUG
 SERVE_STATIC_FILES = False
 
 ADMINS = (
-          ('Filip Varecha', 'xaralis@centrum.cz')
+          ('Jan Wagner', 'wagnejan@fel.cvut.cz.cz')
           )
 
 MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'metrocar_cz'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'metrocar_cz'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
--- mfe/config/settings_admin.py
+++ mfe/config/settings_admin.py
@@ -2,12 +2,6 @@
 
 from settings_dev import *
 
-#DJANGO_BRANCH = 'trunk'
-#DJANGO_REVISION = 11368
-#DJANGO_PATH = '/usr/lib/pymodules/python2.6/django/'
-
-#EXTEND_PYTHONPATH = '/home/bohous/projects/metrocar/pythonpath'
-
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'metrocar'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'metrocar'             # Not used with sqlite3.
@@ -16,3 +10,8 @@ DATABASE_PASSWORD = '4ut1ck4'         # Not used with sqlite3.
 SERVE_STATIC_FILES = True
 MEDIA_ROOT = '/home/komarem/metrocar/static/'
 STATIC_DOC_ROOT = '/home/komarem/metrocar/static/'
+
+import socket
+if socket.gethostname() != 'honca':
+    DEBUG = False
+    TEMPLATE_DEBUG = DEBUG
