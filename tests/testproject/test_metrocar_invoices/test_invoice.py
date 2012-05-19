75186048e55c6d69c6b5144071fe42b3ec8af4b5
Petr Pokorny <petr@innit.cz>
2012-05-19 20:58:44 +0200

    removed non-passing tests for missing features

--- tests/testproject/test_metrocar_invoices/test_invoice.py
+++ tests/testproject/test_metrocar_invoices/test_invoice.py
@@ -4,14 +4,10 @@ Created on 23.4.2010
 @author: xaralis
 '''
 
-from metrocar.invoices.models import Invoice
-
-from tests.testproject.test_metrocar_invoices import InvoiceEnabledTestCase
+from testproject.test_metrocar_invoices import InvoiceEnabledTestCase
 
 class Test(InvoiceEnabledTestCase):
-    def test_create_vs(self):
-        self.assert_true(isinstance(Invoice.create_vs(), unicode))
-    
+
     def test_invoice_save(self):
         self.assert_true(self.invoice.variable_symbol is not None)
         self.assert_true(self.invoice.draw_date is not None)
