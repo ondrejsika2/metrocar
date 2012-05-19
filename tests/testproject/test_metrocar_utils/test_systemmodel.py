976cd1292f2d3e408b6b4f400aa7477401b2fc55
Petr Pokorny <petr@innit.cz>
2012-05-19 22:10:58 +0200

    fixed SystemModel test

--- tests/testproject/test_metrocar_utils/test_systemmodel.py
+++ tests/testproject/test_metrocar_utils/test_systemmodel.py
@@ -1,29 +1,24 @@
-'''
-Created on 23.4.2010
-
-@author: xaralis
-'''
-
-from nose import SkipTest
 from nose.tools import raises
-
-
 from djangosanetesting.cases import DatabaseTestCase
 
-from models import DummyModel
+from testproject.test_metrocar_utils.models import DummyModel
+
 
 class TestSystemModel(DatabaseTestCase):
+
     def setUp(self):
-        super(TestSystemModel, self).setUp()
         self.m = DummyModel(testfield=1)
         self.m.save()
-        
+
     @raises(AssertionError)
     def test_0_not_deletable(self):
         self.m.deleteable = False
         self.m.save()
         self.m.delete()
-        
+
+    @raises(DummyModel.DoesNotExist)
     def test_1_deletable(self):
-        raise SkipTest('Nose compatibility issue')
-        self.assert_true(self.m.delete())
+        pk = self.m.pk
+        assert pk is not None
+        self.m.delete()
+        DummyModel.objects.get(pk=pk)
