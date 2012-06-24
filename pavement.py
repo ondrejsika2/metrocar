2387bf5764656ae1df0c42c192fde00f1d526cc6
Petr Pokorny <petr@innit.cz>
2012-06-24 22:12:38 +0200

    pavement task for deleting .pyc files

--- pavement.py
+++ pavement.py
@@ -1,5 +1,6 @@
 from paver.easy import *
 from paver import doctools
+from paver.path25 import path
 
 
 options(
@@ -15,3 +16,12 @@ def install_dependencies():
     Installs required python packages.
     """
     sh('pip install -r requirements.txt')
+
+
+@task
+def delete_pyc():
+    """
+    Delete all *.pyc files recursively from this directory.
+    """
+    for file in path('.').walkfiles('*.pyc'):
+        file.remove()
