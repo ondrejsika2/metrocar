3a7b165c53c75af94bb69b50924f29b54e92b2cb
Petr Pokorny <petr@innit.cz>
2012-09-26 18:31:52 +0200

    fixed MEDIA_URL setting

--- metrocar/settings/base.py
+++ metrocar/settings/base.py
@@ -70,7 +70,7 @@ MEDIA_ROOT = join(PROJECT_PATH, '..', 'files')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/files/'
 
 
 # Make this unique, and don't share it with anybody.
