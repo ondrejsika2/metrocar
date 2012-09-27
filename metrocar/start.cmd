2bb4852cbae5ed61a71afe40853b0fba16eaccf0
Petr Pokorny <petr@innit.cz>
2012-09-27 13:40:29 +0200

    removed legacy start scripts, we can just use manage.py now

--- metrocar/start.cmd
+++
@@ -1,3 +0,0 @@
-SET PYTHONPATH=D:/www/python/metrocar.cz/libs/django;D:/www/python/metrocar.cz/libs/django_xmlrpc;D:/www/python/metrocar.cz/
-SET DJANGO_SETTINGS_MODULE=metrocar.settings
-manage.py runserver
