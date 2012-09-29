4058dc6561abc1fdfa51a80ad2084ecce093999f
Petr Pokorny <petr@innit.cz>
2012-09-29 15:23:34 +0200

    ...also messages middleware

--- metrocar/settings/base.py
+++ metrocar/settings/base.py
@@ -109,7 +109,8 @@ MIDDLEWARE_CLASSES = (
                       'django.middleware.common.CommonMiddleware',
                       'django.contrib.sessions.middleware.SessionMiddleware',
                       'django.contrib.auth.middleware.AuthenticationMiddleware',
-                      'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
+                      'django.contrib.messages.middleware.MessageMiddleware',
+                      'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
                       )
 
 INSTALLED_APPS = (
