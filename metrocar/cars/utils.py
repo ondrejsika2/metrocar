86e945a116da70f7d8ee02d012141f9a3bdde826
Petr Pokorny <petr@innit.cz>
2012-10-04 23:31:43 +0200

    MEDIA_URL replaced with STATIC_URL for static files

--- metrocar/cars/utils.py
+++ metrocar/cars/utils.py
@@ -14,15 +14,15 @@ def get_car_infomap(width='100%', height='400px'):
     """
     Returns olwidget.widgets.InfoMap instance with available cars on it.
     """
-    return InfoMap([ 
-        [ c.last_position, c.__unicode__() ] for c in Car.objects.all() ], 
+    return InfoMap([
+        [ c.last_position, c.__unicode__() ] for c in Car.objects.all() ],
         options={
             'default_zoom': 8,
             'map_div_style': {'width': width, 'height': height},
             'popupOutside': True,
             'overlayStyle': {
-                'externalGraphic': settings.MEDIA_URL + "img/marker.png",
-                'backgroundGraphic': settings.MEDIA_URL + "img/marker_shadow.png",
+                'externalGraphic': settings.STATIC_URL + "img/marker.png",
+                'backgroundGraphic': settings.STATIC_URL + "img/marker_shadow.png",
                 'graphicXOffset': -10,
                 'graphicYOffset': -12,
                 'graphicWidth': 21,
@@ -32,14 +32,14 @@ def get_car_infomap(width='100%', height='400px'):
             }
         }
     )
-    
+
 def get_map_for_geometry(geometry, width, height):
     """
     Returns basic MapDisplay instance for any GEOS geometry.
     """
     return MapDisplay(
         fields=[ geometry ],
-        options={ 
+        options={
             'default_zoom': 11,
             'map_div_style': {'width': width, 'height': height}
          }
