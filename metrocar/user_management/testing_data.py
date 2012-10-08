d5bed5f8157209182c6bb3dda4552e57bc8e0150
Petr Pokorny <petr@innit.cz>
2012-10-08 22:57:46 +0200

    fix'd dummy admin

--- metrocar/user_management/testing_data.py
+++ metrocar/user_management/testing_data.py
@@ -5,9 +5,11 @@ def create_admin(username='admin', password='admin'):
     admin, created = User.objects.get_or_create(
         username=username,
         is_superuser=True,
+        is_staff=True,
     )
     if created:
         admin.set_password(password)
+        admin.save()
     return admin
 
 
