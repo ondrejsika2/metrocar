622eb22771e177d28c0e66e2ba856d31580f30f1
Petr Pokorny <petr@innit.cz>
2012-10-14 16:19:23 +0200

    paver install_dependencies only installs if requirements file changed

--- pavement.py
+++ pavement.py
@@ -1,6 +1,9 @@
 import re
-from paver.easy import options, Bunch, task, sh
+import os
+from functools import partial
+
 from paver import doctools
+from paver.easy import options, Bunch, task, sh
 from paver.path25 import path
 
 
@@ -15,12 +18,44 @@ def managepy(project, cmd):
     sh('python %s/manage.py %s' % (project, cmd))
 
 
+def read_requirements(filename):
+    """
+    Read pip requirements file and return it's canonical form in a string
+    -- without unnecessary whitespace, comments and sorted alphabetically.
+    """
+    strip_whitespace = partial(map, str.strip)
+    remove_empty_lines = partial(filter, None)
+    strip_comments = partial(filter, lambda line: not line.startswith('#'))
+    return (
+        '\n'.join(
+        sorted(
+        strip_comments(
+        remove_empty_lines(
+        strip_whitespace(
+        open(filename)))))))
+
+
 @task
 def install_dependencies():
     """
     Installs required python packages.
+
+    Only install if the requirements file changed.
     """
-    sh('pip install -r requirements.txt')
+    requirements_file = 'requirements.txt'
+    installed_file = 'requirements.installed'
+    requirements = read_requirements(requirements_file)
+    installed = os.path.exists(installed_file) and open(installed_file).read()
+    if installed == requirements:
+        print ('Nothing new to install. Delete %s if you want to try anyway' %
+            installed_file)
+        return
+
+    sh('pip install -r ' + requirements_file)
+
+    # remember what was installed
+    with open(installed_file, 'w+') as f:
+        f.write(requirements)
 
 
 @task
@@ -51,8 +86,7 @@ def update_db():
 def deploy():
     sh('git pull')
     delete_pyc()
-    # TODO: only run when a new dependency appears
-    # install_dependencies()
+    install_dependencies()
     update_db()
     collectstatic()
     uwsgi_restart()
