28073df95ffd9395e69db631c79c8b92236ecec8
Petr Pokorny <petr@innit.cz>
2012-10-27 22:19:15 +0200

    JsonResponse

--- metrocar/utils/views.py
+++ metrocar/utils/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.utils import simplejson as json
+
+
+def JsonResponse(content, pretty=True, **response_kwargs):
+    kwargs = {'content_type': 'application/json'}
+    kwargs.update(response_kwargs)
+    indent = 2 if pretty else None
+    serialized_content = json.dumps(content,
+        indent=indent,
+        ensure_ascii=False,
+    )
+    return HttpResponse(serialized_content, **kwargs)
