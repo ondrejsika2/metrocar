0063dc684ae7c9aa4cbf9b30a53b77f413cb1a72
Petr Pokorny <petr@innit.cz>
2012-10-31 15:41:56 +0100

    serving media files in development

--- metrocar/urls.py
+++ metrocar/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
 
@@ -19,5 +19,10 @@ urlpatterns = patterns('',
 
 
 if settings.DEBUG:
+    # serving static & media files in development
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}))
