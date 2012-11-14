b1a1751f34d6044372a23de6f752a1492a301ab9
Jakub Ječmínek <jecmijak@gmail.com>
2012-11-14 14:32:03 +0100

    mfe tests will use custom testrunner

--- test_mfe/settings/override.py
+++ test_mfe/settings/override.py
@@ -1,3 +1,10 @@
+from os.path import join, abspath, dirname
+
 from mfe.settings import INSTALLED_APPS
 
 INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'south']
+
+FORCE_SELENIUM_TESTS = True
+
+TEST_RUNNER = 'testrunner.DiscoveryDjangoTestSuiteRunner'
+TEST_DISCOVERY_ROOT = BASE_PATH = abspath(join(dirname(__file__), '..'))
--- test_mfe/test_mfe_users/test_dummy.py
+++ test_mfe/test_mfe_users/test_dummy.py
@@ -1,6 +1,16 @@
 from djangosanetesting.cases import DatabaseTestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
-class TestDummy(DatabaseTestCase):
+class TestDummy(LiveServerTestCase):
+
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()	
 	
 	def test_dumm(self):
 		pass
