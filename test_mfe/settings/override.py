fe3787a17935cce2357259c291da278c99700e74
Petr Pokorny <petr@innit.cz>
2012-11-23 18:50:27 +0100

    mfe tests with locale

--- test_mfe/settings/override.py
+++ test_mfe/settings/override.py
@@ -6,3 +6,7 @@ INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'south']
 
 TEST_RUNNER = 'testrunner.DiscoveryDjangoTestSuiteRunner'
 TEST_DISCOVERY_ROOT = BASE_PATH = abspath(join(dirname(__file__), '..'))
+
+LOCALE_PATHS = [
+    join(BASE_PATH, '..', 'mfe', 'locale'),
+]
