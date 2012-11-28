71268721c2fada12e5aa2a82c4abde0c34602e50
Petr_Holecek <holecpe7@fel.cvut.cz>
2012-11-28 15:03:37 +0100

    Test prihlasovani

--- test_mfe/test_mfe_users/test_login_webtest.py
+++ test_mfe/test_mfe_users/test_login_webtest.py
@@ -1,4 +1,6 @@
+
 # encoding: utf-8
+
 from django_webtest import WebTest
 
 from metrocar.user_management.models import MetrocarUser
@@ -9,7 +11,7 @@ class TestLogin(WebTest):
     def test_valid_login(self):
         username = 'afdasdfadf'
         password = 'sadf68a46d'
-        user = MetrocarUser.objects.create(username=username)
+        user = MetrocarUser.objects.create(username=username, language='CS')
         user.set_password(password)
         user.save()
 
@@ -17,10 +19,18 @@ class TestLogin(WebTest):
         form = page.forms['login_form']
         form['username'] = username
         form['password'] = password
+        
+        # Follow?
         response = form.submit()
         # if the login was successful, we should get a redirect
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.location.endswith('/uzivatele/muj-ucet/'))
+        
+        page = response.follow()
+        
+        
+        # assert na testovani promennych kontextu
+        self.assertEqual(page.context['user'].username, username)
 
     def test_invalid_login(self):
         page = self.app.get('/prihlaseni/')
@@ -29,3 +39,6 @@ class TestLogin(WebTest):
         form['password'] = 'invalid84984'
         response = form.submit()
         self.assertEqual(response.status_code, 200)
+        
+        # Assert na nadpis ze byl spatnej login
+
