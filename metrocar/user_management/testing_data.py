c838c0764cc5b6ea67f6234c2dad965a5d91767a
Petr Pokorny <petr@innit.cz>
2012-11-30 21:36:25 +0100

    added testing_data: MetrocarUser and a Deposit for them

--- metrocar/user_management/testing_data.py
+++ metrocar/user_management/testing_data.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import User
+
+from metrocar.user_management.models import MetrocarUser, Deposit, Account
 
 
 def create_admin(username='admin', password='admin'):
@@ -13,7 +16,46 @@ def create_admin(username='admin', password='admin'):
     return admin
 
 
+def create_user(username, password, first_name, last_name, email=None, **kwargs):
+    id = dict(username=username)
+
+    if (User.objects.filter(**id).exists()
+        and not MetrocarUser.objects.filter(**id).exists()):
+        User.objects.get(**id).delete()
+
+    defaults = dict(
+        first_name=first_name,
+        last_name=last_name,
+        email=email or '%s@mailinator.com' % username,
+        drivers_licence_number='0000000',
+        gender='M',
+        identity_card_number='0000000',
+        primary_phone='000 000000000',
+        language=settings.LANG_CHOICES[0][0],
+    )
+    user = MetrocarUser.objects.get_or_create(
+        defaults=dict(defaults, **kwargs), **id)[0]
+    user.set_password(password)
+    user.save()
+    return user
+
+
+def create_deposit(username, amount, comment='Testing deposit', **kwargs):
+    account = Account.objects.get(user__username=username)
+    return Deposit.objects.get_or_create(
+        account=account,
+        money_amount=amount,
+        comment=comment,
+        defaults=kwargs)[0]
+
+
 def create():
     return {
-        'users': [create_admin()],
+        'users': [
+            create_admin(),
+            create_user('test', 'test', 'John', 'Dope'),
+        ],
+        'deposits': [
+            create_deposit('test', 9000),
+        ]
     }
