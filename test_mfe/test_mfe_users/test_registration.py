470ed2778e67198966d22aa5540d0c6e2d1808d4
Petr Pokorny <petr@innit.cz>
2012-12-07 22:52:22 +0100

    fixed registration test (indentation, skip if selenium isnt available)

--- test_mfe/test_mfe_users/test_registration.py
+++ test_mfe/test_mfe_users/test_registration.py
@@ -1,45 +1,50 @@
 from django.test import LiveServerTestCase
+from django.utils.unittest import SkipTest
+
 from selenium import webdriver
-from django.utils.translation import gettext_lazy as _
-from django.template.defaultfilters import slugify
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
 
 class TestRegistration(LiveServerTestCase):
 
+    def setUp(self):
+        try:
+            self.browser = webdriver.Firefox()
+        except WebDriverException:
+            raise SkipTest('unable to start Selenium')
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
-	
-	def test_registration(self):
-		self.browser.get(self.live_server_url + '/registration/')
-		headings = self.browser.find_elements_by_tag_name('h1')			
-		self.assertEquals(headings[1].text, 'Registration')
-		username_field = self.browser.find_element_by_id('id_username')
-		username_field.send_keys('testUser')
-		password_field = self.browser.find_element_by_id('id_password')
-		password_field.send_keys('password')
-		password_confirm_field = self.browser.find_element_by_id('id_password2')
-		password_confirm_field.send_keys('password')
-		first_name_field = self.browser.find_element_by_id('id_first_name')
-		first_name_field.send_keys('Pokusny')
-		last_name_filed = self.browser.find_element_by_id('id_last_name')
-		last_name_filed.send_keys('Uzivatel')
-		email_field = self.browser.find_element_by_id('id_email')
-		email_field.send_keys('pokusny@fel.cvut.cz')
-		primary_phone_field = self.browser.find_element_by_id('id_primary_phone')
-		primary_phone_field.send_keys('123 123456789')
-		date_of_birth_field = self.browser.find_element_by_id('id_date_of_birth')
-		date_of_birth_field.send_keys('1990-02-09')
-		drivers_licence_number_field = self.browser.find_element_by_id('id_drivers_licence_number')
-		drivers_licence_number_field.send_keys('123123123')
-		identity_card_number_field = self.browser.find_element_by_id('id_identity_card_number')
-		identity_card_number_field.send_keys('123123123')
-		identity_card_number_field.send_keys(Keys.RETURN)
-		succes_value = u"You have been successfully registered. You will be sent an e-mail as soon as our administrators validate your registration request."
-		succes = self.browser.find_element_by_css_selector("li[class='success']")
+    def tearDown(self):
+        self.browser.quit()
 
-		self.assertEquals(succes.text, succes_value)
+    def test_registration(self):
+        self.browser.get(self.live_server_url + '/registration/')
+        headings = self.browser.find_elements_by_tag_name('h1')
+        self.assertEquals(headings[1].text, 'Registration')
+        username_field = self.browser.find_element_by_id('id_username')
+        username_field.send_keys('testUser')
+        password_field = self.browser.find_element_by_id('id_password')
+        password_field.send_keys('password')
+        password_confirm_field = self.browser.find_element_by_id('id_password2')
+        password_confirm_field.send_keys('password')
+        first_name_field = self.browser.find_element_by_id('id_first_name')
+        first_name_field.send_keys('Pokusny')
+        last_name_filed = self.browser.find_element_by_id('id_last_name')
+        last_name_filed.send_keys('Uzivatel')
+        email_field = self.browser.find_element_by_id('id_email')
+        email_field.send_keys('pokusny@fel.cvut.cz')
+        primary_phone_field = self.browser.find_element_by_id('id_primary_phone')
+        primary_phone_field.send_keys('123 123456789')
+        date_of_birth_field = self.browser.find_element_by_id('id_date_of_birth')
+        date_of_birth_field.send_keys('1990-02-09')
+        drivers_licence_number_field = self.browser.find_element_by_id('id_drivers_licence_number')
+        drivers_licence_number_field.send_keys('123123123')
+        identity_card_number_field = self.browser.find_element_by_id('id_identity_card_number')
+        identity_card_number_field.send_keys('123123123')
+        identity_card_number_field.send_keys(Keys.RETURN)
+        succes_value = u"You have been successfully registered. You will be sent an e-mail as soon as our administrators validate your registration request."
+        succes = self.browser.find_element_by_css_selector("li[class='success']")
+
+        self.assertEquals(succes.text, succes_value)
