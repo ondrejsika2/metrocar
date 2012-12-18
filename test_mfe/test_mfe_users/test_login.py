6a3aa56254f2dda2b1345a9a4008c20492c80dc7
Petr Pokorny <petr@innit.cz>
2012-12-18 16:49:32 +0100

    skip test when Selenium isn't available, pep8

--- test_mfe/test_mfe_users/test_login.py
+++ test_mfe/test_mfe_users/test_login.py
@@ -1,33 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.firefox.webdriver import WebDriver
-from django.utils.translation import gettext_lazy as _
-from django.template.defaultfilters import slugify
-from selenium.webdriver.common.keys import Keys
-from metrocar.user_management.models import MetrocarUser
+from django.utils.unittest import SkipTest
 
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+
+from metrocar.user_management.models import MetrocarUser
 
 
 class TestLogin(LiveServerTestCase):
     fixtures = ['user.json', 'user'] # Nefunguje
     fixtures = ['user2.json', 'user2'] # Auch nicht
-    
+
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        try:
+            self.browser = webdriver.Firefox()
+        except WebDriverException:
+            raise SkipTest('unable to start Selenium')
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_login(self):
-    
+
         username = 'test'
         password = 'password'
-        
+
         user = MetrocarUser.objects.create(username=username)
- 
+
         user.set_password(password)
         user.password2 = password
         user.first_name = 'Pokusny'
@@ -39,16 +42,15 @@ class TestLogin(LiveServerTestCase):
         user.identity_card_number = '123123123'
         user.language = 'CS'
         user.save()
-        
 
         self.browser.get(self.live_server_url + '/prihlaseni/')
         headings = self.browser.find_elements_by_tag_name('h1')
 
         # Podle ./mfe/locale/cs/LC_MESSAGES/django.po
         self.assertEquals(headings[1].text, u"Přihlášení je vyžadováno")
-        
+
         self.browser.find_element_by_id('j-open-login-box').click()
-        
+
         username_field = self.browser.find_element_by_id('id_login-username')
         username_field.clear()
         username_field.send_keys('test')
@@ -56,19 +58,17 @@ class TestLogin(LiveServerTestCase):
         password_field.clear()
         password_field.send_keys('password')
         password_field.send_keys(Keys.RETURN)
-        
+
         # Podle ./mfe/locale/cs/LC_MESSAGES/django.po
         succes_value = u"Byli jste úspěšně přihlášeni."
 
         # Nas pripad
         succes = self.browser.find_element_by_css_selector("li[class='success']")
-        
+
         # Otestovani hodnot v detailech - je nutne zadat IDcka u seznamu nebo to prepsat,
         # nebo umi Selenium vyhledavat jenom String fulltextove
         # Neco jako
         # self.assertEquals(self.browser.find_element_by_neco, user.username)
         # popr dalsi
-        
 
         self.assertEquals(succes.text, succes_value)
-
