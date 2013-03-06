651291a596fc088b2af9d8e4013532b41601ffdd
Jakub Ječmínek <jecmijak@gmail.com>
2013-03-06 19:37:32 +0100

    zmenen system hashovani hesel

--- metrocar/settings/base.py
+++ metrocar/settings/base.py
@@ -149,6 +149,16 @@ INSTALLED_APPS = (
     'metrocar.tests',
 )
 
+#password hassher set to SHA1 because of communication with car_units
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',    
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
 # Will this instance make use of geo-data from car units
 GEO_ENABLED = True
 
