5cb5cdbaaa3cc8a95b3f944108aaf12a3981f6cf
Jakub Ječmínek <jecmijak@gmail.com>
2013-04-25 20:42:48 +0200

    vyresen problem s danemi

--- metrocar/accounting/api.py
+++ metrocar/accounting/api.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+import invoices_management
+
+
+def check_incoming_payments():
+	invoices_management.check_incoming_payments()
+
+def create_invoice(inv):
+	invoices_management.create_invoice(inv)	
+
+def print_invoice(inv):
+	invoices_management.print_invoice(inv)	
+
+def delete_invoice_receiver():
+	invoices_management.delete_invoice_receiver
