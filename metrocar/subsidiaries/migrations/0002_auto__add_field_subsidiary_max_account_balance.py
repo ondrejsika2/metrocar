15a449787e3db72f871f85977c5b225fa973875f
Jakub Ječmínek <jecmijak@gmail.com>
2013-05-05 17:51:14 +0200

    pridana jedna schema migrace a upraven form na kredit

---
+++ metrocar/subsidiaries/migrations/0002_auto__add_field_subsidiary_max_account_balance.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Subsidiary.max_account_balance'
+        db.add_column('subsidiaries_subsidiary', 'max_account_balance',
+                      self.gf('django.db.models.fields.DecimalField')(default='15000', max_digits=8, decimal_places=2),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Subsidiary.max_account_balance'
+        db.delete_column('subsidiaries_subsidiary', 'max_account_balance')
+
+
+    models = {
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'subsidiaries.subsidiary': {
+            'Meta': {'object_name': 'Subsidiary'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'house_number': ('django.db.models.fields.IntegerField', [], {'max_length': '8'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'max_account_balance': ('django.db.models.fields.DecimalField', [], {'default': "'15000'", 'max_digits': '8', 'decimal_places': '2'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'site': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['sites.Site']", 'unique': 'True'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'tax_rate': ('django.db.models.fields.FloatField', [], {'default': '21'}),
+            'use_onboard_unit': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        }
+    }
+
+    complete_apps = ['subsidiaries']
