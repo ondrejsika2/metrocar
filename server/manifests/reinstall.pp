8d81d8bdeb5c710dc27f44279bd5b6baf5cc2e47
Tomas Jungman <info@teejays.cz>
2016-05-15 15:34:18 +0200

    puppet - reinstall prod app

--- server/manifests/reinstall.pp
+++ server/manifests/reinstall.pp
@@ -53,3 +53,41 @@ exec { 'py_manage_dummy_data':
   command => 'python manage.py load_dummy_data',
   cwd => '/home/metrocar/repo/server/metrocar/',
 }
+
+# ----- mark default settings file
+
+file { '/home/metrocar/repo/server/metrocar/settings/local.py':
+  ensure => 'present',
+  source => '/home/metrocar/repo/server/metrocar/settings/local_example.py',
+}
+->
+
+# ----- build python apps
+
+exec { 'install_geotrack':
+  command => 'pip install -e /home/metrocar/repo/geotrack/',
+  cwd    => '/home/metrocar/repo/',
+}
+->
+exec { 'install_metrocar':
+  command => 'pip install -e /home/metrocar/repo/server/',
+  cwd    => '/home/metrocar/repo/',
+}
+->
+exec { 'install_metrocar_requirements':
+  command => 'pip install -r /home/metrocar/repo/server/requirements.txt',
+  cwd    => '/home/metrocar/repo/',
+}
+->
+
+# ----- restart apache
+
+exec { 'enable_site':
+  command => 'a2ensite metrocar.jezdito.cz',
+  cwd => "/",
+}
+->
+exec { 'restart_apache':
+  command => 'service apache2 reload',
+  cwd     => "/",
+}
